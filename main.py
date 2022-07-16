9f3ef5b114ef5cdd29260c270b0eec6449937d5a
Sylwester <sylwestronowak@gmail.com>
2022-07-16 20:31:15 +0200

    starting

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from matplotlib.pyplot import text
 import requests
 
 text_html = requests.get('https://www.olx.pl/d/motoryzacja/samochody/?search%5Bfilter_float_price:from%5D=10000&search%5Bfilter_float_price:to%5D=50000&search%5Bfilter_float_year:from%5D=2010')
