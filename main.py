32c439edb14bd9299abe35cbc6f0618c2bebb1c4
Sylwester <sylwestronowak@gmail.com>
2022-07-16 22:30:18 +0200

    Test commit

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import requests
 text_html = requests.get('https://www.olx.pl/d/motoryzacja/samochody/?search%5Bfilter_float_price:from%5D=10000&search%5Bfilter_float_price:to%5D=50000&search%5Bfilter_float_year:from%5D=2010')
 
 print(text_html)
+
+print("alle")
