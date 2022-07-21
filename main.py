2de7634fbddb64cb213db0a32737f088c90fd6f7
Sylwester <sylwestronowak@gmail.com>
2022-07-21 14:43:07 +0200

    functions created

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import unicodedata
 import csv
 import lxml.html
 
+
 # Connecting to otodom list cities to create list of available cities. Based on that we can later exctract city from common class which include other strings.
 with open('cities.csv', encoding="utf8") as cities_file:
     reader = csv.reader(cities_file)
@@ -14,36 +15,33 @@ with open('cities.csv', encoding="utf8") as cities_file:
         for city in city_listed:
             if city != '':
                 cities_list.append(city)
-    
 
 
-# Connect to site, create soup
-
-post_titles, post_prices, post_cities, post_sqmetrage, post_rooms, post_type  = ([] for i in range(6))
+# Connect to page
 all_pages_html = ''
-
-for page in range(0,5):
+for page in range(0,10):
     URL_Site = 'https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/cala-polska?market=ALL&viewType=listing&lang=pl&searchingCriteria=sprzedaz&searchingCriteria=mieszkanie&page={}'.format(page)
     req = requests.get(URL_Site).text
     all_pages_html = all_pages_html + req[:-7] #-7 to remove </html> as lxml parser doesn't work properly with it.
 
-main_soup = BeautifulSoup(all_pages_html, 'lxml')
 
-# Post containers and empty lists to which we're appending info scrapped. Later we will use the lists to create pandas table
-post_container = main_soup.find_all('article', class_ = 'css-1th7s4x es62z2j16')
+#Empty arrays to store data
+post_titles, post_prices, post_cities, post_sqmetrage, post_rooms, post_type  = ([] for i in range(6))
 
-# Loop to dive into post container and extract informations
-for post in post_container:
-    # 1. Find & append titles
+
+#Functions
+# 1. Find & append titles
+def append_titles():
     post_title = post.find('h3', class_ = 'css-1rhznz4 es62z2j11').text
     post_titles.append(post_title)
 
-    # 2. Find & append prices, number of rooms, square metrage (they're in same span class).
+# 2. Find & append prices, number of rooms, square metrage (they're in same span class).
     # Loop for replacements in price. Inserting None instead of "ask for price" if there's no price mentioned. 
+def append_prices():    
     post_price = post.find('span', class_ = 'css-rmqm02 eclomwz0').text
     post_price_ns = unicodedata.normalize('NFKD', post_price)
 
-    post_price_replace = {' ':'', 'zł': '','€':'', ',':'.'}
+    post_price_replace = {' ':'', 'zł': '','€':'','$':'', ',':'.'}
     for key, value in post_price_replace.items():
         post_price_ns = post_price_ns.replace(key, value)
 
@@ -52,7 +50,8 @@ for post in post_container:
     else:
         post_prices.append(round(int(float(post_price_ns)), 0))
 
-    # 3. Find & append cities in common class span. Comparing with goverment list of polish cities.
+# 3. Find & append cities in common class span. Comparing with goverment list of polish cities.
+def append_cities():
     post_area = post.find('span', class_ = 'css-17o293g es62z2j9').text.split(',')
     for town in post_area:
         if town.strip() in cities_list:
@@ -61,17 +60,35 @@ for post in post_container:
         else:
             continue
 
-    # 4/5. Find & append square metrage in common class span.
+# 4/5. Find & append square metrage and rooms in common class span.
+def append_sqm():
     post_sq_rooms = post.find_all('span', class_='css-rmqm02 eclomwz0')
     post_rooms.append(int(post_sq_rooms[2].text[0]))
     post_sqmetrage.append(float(post_sq_rooms[3].text.split(' ')[0]))
 
-    # 6. Find & append type of post. Can be private or company.
-    post_sq_rooms = post.find('span', class_='css-13vzu28 e1dxhs6v2')
-    if post_sq_rooms != None:
-        post_type.append(post_sq_rooms.text)
+# 6. Find & append type of post. Can be private or company.
+def append_type():
+    post_sq_type = post.find('span', class_='css-13vzu28 e1dxhs6v2')
+    if post_sq_type != None:
+        post_type.append(post_sq_type.text)
     else:
         post_type.append('Oferta firmowa')
+
+
+
+main_soup = BeautifulSoup(all_pages_html, 'lxml')
+
+# Post containers and empty lists to which we're appending info scrapped. Later we will use the lists to create pandas table
+post_container = main_soup.find_all('article', class_ = 'css-1th7s4x es62z2j16')
+
+# Loop to dive into post container and extract informations
+for post in post_container:
+    append_titles()
+    append_prices()
+    append_cities()
+    append_sqm()
+    append_type()
+
 
 print(len(post_titles))
 print(len(post_cities))
@@ -80,4 +97,6 @@ print(len(post_rooms))
 print(len(post_sqmetrage))
 print(len(post_type))
 
-# Podłączyć wszystkie strony
+
+
+#przyspieszyć działanie
