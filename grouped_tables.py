b62b3e4243ced7433c887015a3c2957449b249d3
Sylwester <sylwestronowak@gmail.com>
2022-08-15 22:33:01 +0200

    city grouping completed

--- grouped_tables.py
+++ grouped_tables.py
@@ -1,5 +1,6 @@
 import pyspark
 from pyspark.sql import SparkSession, functions as pyspf
+from pyspark.sql.functions import lit, current_date
 import os
 
 mysql_username = os.environ.get('MYSQL_USERNAME')
@@ -16,8 +17,12 @@ all_data_df = spark.read.format('jdbc') \
                     .option("dbtable","mieszkania") \
                     .load()
 
-all_data_df.groupBy('post_city', 'rodzaj').agg(pyspf.round(pyspf.avg('post_price'), 0).alias('avg_price'), pyspf.min('post_price').alias('min_price'), pyspf.max('post_price').alias('max_price'), \
+city_grouped = all_data_df.groupBy('post_city', 'rodzaj').agg(pyspf.round(pyspf.avg('post_price'), 0).alias('avg_price'), pyspf.min('post_price').alias('min_price'), pyspf.max('post_price').alias('max_price'), \
                                             pyspf.round(pyspf.avg('post_rooms'), 0).alias('avg_rooms'), \
                                             pyspf.round(pyspf.avg('post_sqmetrage'), 0).alias('avg_sqmetrage'), pyspf.min('post_sqmetrage').alias('min_sqmetrage'), pyspf.max('post_sqmetrage').alias('max_sqmetrage'), \
                                             pyspf.round(pyspf.avg('price_per_sqm'), 0).alias('avg_price_sqm'), pyspf.min('price_per_sqm').alias('min_price_sqm'), pyspf.max('price_per_sqm').alias('max_price_sqm') \
-                                            ).show()
+                                            ).withColumn('download_date', lit(current_date())).show()
+
+# Teraz groupowanie na miasta wojewódzkie
+
+
