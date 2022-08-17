95eb054881e43f199e2b165b31615fbe3e5482ce
Sylwester <sylwestronowak@gmail.com>
2022-08-17 18:34:50 +0200

    df grouped tables cleaned commented

--- grouped_tables.py
+++ grouped_tables.py
@@ -17,12 +17,15 @@ all_data_df = spark.read.format('jdbc') \
                     .option("dbtable","mieszkania") \
                     .load()
 
+# Df with data grouped by city and post type (rent/sale). Details.
 city_grouped = all_data_df.groupBy('post_city', 'rodzaj').agg(pyspf.round(pyspf.avg('post_price'), 0).alias('avg_price'), pyspf.min('post_price').alias('min_price'), pyspf.max('post_price').alias('max_price'), \
                                             pyspf.round(pyspf.avg('post_rooms'), 0).alias('avg_rooms'), \
                                             pyspf.round(pyspf.avg('post_sqmetrage'), 0).alias('avg_sqmetrage'), pyspf.min('post_sqmetrage').alias('min_sqmetrage'), pyspf.max('post_sqmetrage').alias('max_sqmetrage'), \
                                             pyspf.round(pyspf.avg('price_per_sqm'), 0).alias('avg_price_sqm'), pyspf.min('price_per_sqm').alias('min_price_sqm'), pyspf.max('price_per_sqm').alias('max_price_sqm') \
                                             ).withColumn('download_date', lit(current_date())).show()
 
-# Teraz groupowanie na miasta wojewódzkie
+
+# List of main cities in PL. Df with city_grouped df filtered by them.
+
 
 
